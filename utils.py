bb396e9136e89d9dd3f9b181a6236543627bda70
Melissa Blades <melissa@saltymermaid.dev>
2024-12-06 03:59:52 -0500

    Day 4

--- utils.py
+++ utils.py
@@ -5,7 +5,10 @@ def get_input_lines(puzzle, test):
         data_type = "data"
     filename = f"advent{puzzle}_{data_type}.txt"
     with open(filename) as f:
-        test_data = f.readlines()
+        input_data = f.readlines()
+    test_data = []
+    for row in input_data:
+        test_data.append(row.strip())
     return test_data
 
 def get_input_string(puzzle, test):
